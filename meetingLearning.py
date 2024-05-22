6e1bb2f1fc173f59d2842642f8eb9a5062fa9282
nwj <1225708606@qq.com>
2024-05-22 15:25:31 +0800

    v1.1

--- meetingLearning.py
+++ meetingLearning.py
@@ -7,49 +7,49 @@ def meetingFile_AI():
     st.markdown("""
 ### Qcon2024:
 
-- **文件名称:** 向量数据库与+RAG+落地思考与实践-苏鹏.pdf 分享链接:https://pan.baidu.com/s/14Sj6mkrGC8Vf8MD1l05z9A 提取码:AIGC  
+- **文件名称:** 向量数据库与+RAG+落地思考与实践-苏鹏.pdf 分享链接:https://pan.baidu.com/s/14Sj6mkrGC8Vf8MD1l05z9A  
 
-- **文件名称:** 加速+AI+模型生产化：eBay+交易风控+AI+模型推理仿真设计和实现-王兵.pdf 分享链接:https://pan.baidu.com/s/1oNNfDzZF-IoEmvXGbE5bWA 提取码:AIGC  
+- **文件名称:** 加速+AI+模型生产化：eBay+交易风控+AI+模型推理仿真设计和实现-王兵.pdf 分享链接:https://pan.baidu.com/s/1oNNfDzZF-IoEmvXGbE5bWA  
 
 - **文件名称:** Al+原生数据库+Infinity+系统架构与+RAG+技术实践-张颖峰.pdf 分享链接:https://pan.baidu.com/s/1B-h1dgP3WedzqLUSrw6lAQ 提取码:AIGC 
 
-- **文件名称:** 基于+AI+大模型生成微信小程序的探索与实践-骆勤.pdf 分享链接:https://pan.baidu.com/s/1KrKhkfmVdqA3VQdwN65bow 提取码:AIGC  
+- **文件名称:** 基于+AI+大模型生成微信小程序的探索与实践-骆勤.pdf 分享链接:https://pan.baidu.com/s/1KrKhkfmVdqA3VQdwN65bow  
 
-- **文件名称:** 大模型原生应用产品设计的前沿探索-岑润哲.pdf 分享链接:https://pan.baidu.com/s/1891E95O6Bp-76YfGcY-L1g 提取码:AIGC  
+- **文件名称:** 大模型原生应用产品设计的前沿探索-岑润哲.pdf 分享链接:https://pan.baidu.com/s/1891E95O6Bp-76YfGcY-L1g  
 
-- **文件名称:** LLM+和+Multi-agent+在运维领域的实验探索-张曦+.pdf 分享链接:https://pan.baidu.com/s/11EvC69S2AXMGWA-5gTJsPQ 提取码:AIGC  
+- **文件名称:** LLM+和+Multi-agent+在运维领域的实验探索-张曦+.pdf 分享链接:https://pan.baidu.com/s/11EvC69S2AXMGWA-5gTJsPQ  
 
-- **文件名称:** 面向办公自动化领域的+AI+Agent+建设思考与分享-欧阳小刚.pdf 分享链接:https://pan.baidu.com/s/1dNWOykzKbuLtkrEhtiP79w 提取码:AIGC  
+- **文件名称:** 面向办公自动化领域的+AI+Agent+建设思考与分享-欧阳小刚.pdf 分享链接:https://pan.baidu.com/s/1dNWOykzKbuLtkrEhtiP79w  
 
-- **文件名称:** 金山云：大模型推动知识工作领域的创新与变革-陈海彪.pdf 分享链接:https://pan.baidu.com/s/1nuDkEJWeLk-H0IZ_VAlX1g 提取码:AIGC  
+- **文件名称:** 金山云：大模型推动知识工作领域的创新与变革-陈海彪.pdf 分享链接:https://pan.baidu.com/s/1nuDkEJWeLk-H0IZ_VAlX1g  
 
-- **文件名称:** 大模型技术在快手搜索的应用-许坤.pdf 分享链接:https://pan.baidu.com/s/1Svnop6e2q83bpTZ5e3iRtw 提取码:AIGC  
+- **文件名称:** 大模型技术在快手搜索的应用-许坤.pdf 分享链接:https://pan.baidu.com/s/1Svnop6e2q83bpTZ5e3iRtw  
 
-- **文件名称:** 教育大模型，说你行你才行-郭家.pdf 分享链接:https://pan.baidu.com/s/1tku_UxCnMf9BvyKV7-Rfcg 提取码:AIGC  
+- **文件名称:** 教育大模型，说你行你才行-郭家.pdf 分享链接:https://pan.baidu.com/s/1tku_UxCnMf9BvyKV7-Rfcg  
 
-- **文件名称**: 大模型数据安全：从测评到实时检测的全流程实践-申书恒.pdf 分享链接:https://pan.baidu.com/s/14LkgpRh0VzDRMeAJ8ipusQ 提取码:AIGC  
+-- **文件名称:**  大模型数据安全：从测评到实时检测的全流程实践-申书恒.pdf 分享链接:https://pan.baidu.com/s/14LkgpRh0VzDRMeAJ8ipusQ  
 
-- **文件名称:** RAG+商业落地实践踩坑-高原.pdf 分享链接:https://pan.baidu.com/s/1Bw0jyEIdNYYFnWD8oA5WLA 提取码:AIGC  
+- **文件名称:** RAG+商业落地实践踩坑-高原.pdf 分享链接:https://pan.baidu.com/s/1Bw0jyEIdNYYFnWD8oA5WLA  
 
-- **文件名称:** eBPF+++LLM：实现可观测性智能体的基础设施-向阳.pdf 分享链接:https://pan.baidu.com/s/1WBBOtgzpL1tAD8VgCb58mQ 提取码:AIGC  
+- **文件名称:** eBPF+++LLM：实现可观测性智能体的基础设施-向阳.pdf 分享链接:https://pan.baidu.com/s/1WBBOtgzpL1tAD8VgCb58mQ  
 
-- **文件名称:** 大模型的三次触动、两个难题、一个参考答案-贾安亚.pdf 分享链接:https://pan.baidu.com/s/1jaE5bHiDp-pKwmLNXtG2Ug 提取码:AIGC  
+- **文件名称:** 大模型的三次触动、两个难题、一个参考答案-贾安亚.pdf 分享链接:https://pan.baidu.com/s/1jaE5bHiDp-pKwmLNXtG2Ug  
 
-- **文件名称:** 看不见的大猩猩&mdash;&mdash;智能时代的企业生存和发展之路-韦青.pdf 分享链接:https://pan.baidu.com/s/1F8F-iySn7Lkq6T_HBIBBZA 提取码:AIGC  
+- **文件名称:** 看不见的大猩猩&mdash;&mdash;智能时代的企业生存和发展之路-韦青.pdf 分享链接:https://pan.baidu.com/s/1F8F-iySn7Lkq6T_HBIBBZA  
 
-- **文件名称:** AI+大模型技术在数据库+DevOps+的实践-叶正盛.pdf 分享链接:https://pan.baidu.com/s/19QT82-2wHsJY-qPyo3Hd8g 提取码:AIGC  
+- **文件名称:** AI+大模型技术在数据库+DevOps+的实践-叶正盛.pdf 分享链接:https://pan.baidu.com/s/19QT82-2wHsJY-qPyo3Hd8g  
 
-- **文件名称:** SRE－Copliot：基于大语言模型的智能运维架构-王宁.pdf 分享链接:https://pan.baidu.com/s/1ii0yDmLIW3xpiLbGsqik1w 提取码:AIGC  
+- **文件名称:** SRE－Copliot：基于大语言模型的智能运维架构-王宁.pdf 分享链接:https://pan.baidu.com/s/1ii0yDmLIW3xpiLbGsqik1w  
 
-- **文件名称:** 大模型应用开发新范式-娄双双.pdf 分享链接:https://pan.baidu.com/s/1ao8WbyT-iJiPl_9JfbGHAw 提取码:AIGC  
+- **文件名称:** 大模型应用开发新范式-娄双双.pdf 分享链接:https://pan.baidu.com/s/1ao8WbyT-iJiPl_9JfbGHAw  
 
-- **文件名称:** AI+Agent+在企业经营分析场景的落地-数势科技李飞.pdf 分享链接:https://pan.baidu.com/s/1hnULiywhaVQWwE97MrthyA 提取码:AIGC  
+- **文件名称:** AI+Agent+在企业经营分析场景的落地-数势科技李飞.pdf 分享链接:https://pan.baidu.com/s/1hnULiywhaVQWwE97MrthyA  
 
-- **文件名称:** 基于多模态智能引擎的大模型知识库技术应用-张红兵.pdf 分享链接:https://pan.baidu.com/s/1lMSSk9PP_ZphVEo2b66TNw 提取码:AIGC  
+- **文件名称:** 基于多模态智能引擎的大模型知识库技术应用-张红兵.pdf 分享链接:https://pan.baidu.com/s/1lMSSk9PP_ZphVEo2b66TNw  
 
 - **文件名称:** 激活数据潜能：向量数据库与生成式AI的实战洞察-诸葛瑞麟.pdf 分享链接:https://pan.baidu.com/s/1MaFuhzSIsJTCvEuZmTRXFQ 提取码:AIGC 
 
-- **文件名称:** 蚂蚁智能可观测+Mpilot+AI+助手落地实践-陈真.pdf 分享链接:https://pan.baidu.com/s/11k7JcvKyuzsHLDdQfHe2eg 提取码:AIGC  
+- **文件名称:** 蚂蚁智能可观测+Mpilot+AI+助手落地实践-陈真.pdf 分享链接:https://pan.baidu.com/s/11k7JcvKyuzsHLDdQfHe2eg  
 
 
 """, unsafe_allow_html=True)
@@ -59,40 +59,40 @@ def meetingFile_softTest():
     # 标题
     st.header("🔗峰会资料下载_软件测试（2023）", divider="rainbow")
     st.markdown("""
-**文件名称：**测试专场 分享链接:https://pan.baidu.com/s/1D-Q0Bkhd7wTk7q5j3xC6OA  
-**文件名称：**A2--魏同权--基于大模型的软件测试能力提升研究.pdf 分享链接:https://pan.baidu.com/s/1otDDroWUbvueFf_6J3v0Pg  
-**文件名称：**A2--魏同权--基于大模型的软件测试能力提升研究(1).pdf 分享链接:https://pan.baidu.com/s/1epUtJHnUZ0_g6jHd1vOAUw  
-**文件名称：**A1--王璐璐--坚持追求卓越 - 软件测试的自我革新与实践.pdf 分享链接:https://pan.baidu.com/s/1nKwvN5oB1PSLWMS99gRhtQ  
-**文件名称：**A3--丁海苗--基于录制回放的智能化测试在金融场景的落地及实践(1).pdf 分享链接:https://pan.baidu.com/s/12sLcEpnjgJ7HqyjxcBZpww  
-**文件名称：**A1--林冰玉--延伸测试边界，构建银行新型质量体系(1).pdf 分享链接:https://pan.baidu.com/s/1-8jxnRpF-yc0EDL-knDgFg  
-**文件名称：**A1--王璐璐--坚持追求卓越 - 软件测试的自我革新与实践(2).pdf 分享链接:https://pan.baidu.com/s/1DL5nhPKFRFrBtPTFQzxoBQ  
-**文件名称：**A3-余超-多云环境中部署及管理Kubernetes 集群的测试自动化实现.pdf 分享链接:https://pan.baidu.com/s/1qUHSbmUlVRX4iB81m1u23A  
-**文件名称：**A4--吕飞飞--基于LLM提升测试效率的应用实践(1).pdf 分享链接:https://pan.baidu.com/s/1qD7-avi7aSOpysSzFAerFA  
-**文件名称：**自动化测试专场--余超--多云环境中部署及管理K8S集群的测试自动化实现-脱敏(1).pdf 分享链接:https://pan.baidu.com/s/1jNASLJ5fvqiTQ8edxEIu7g  
-**文件名称：**自动化测试专场--余超--多云环境中部署及管理K8S集群的测试自动化实现-脱敏.pdf 分享链接:https://pan.baidu.com/s/1sSMhP3yNYKRYuhhtB-ydWw  
-**文件名称：**A1-李柯-汽车数字转型下的智能精准测试.pdf 分享链接:https://pan.baidu.com/s/1wXjSP3Brx3YO3vTrD9UdBw  
-**文件名称：**A2--周海莲--基于AIGC的蚂蚁新一代测试用例自动生成技术.pdf 分享链接:https://pan.baidu.com/s/1L7Tz80m2U3pM59xAR-q7jg  
-**文件名称：**A4--吕飞飞--基于LLM提升测试效率的应用实践.pdf 分享链接:https://pan.baidu.com/s/15eAdNeH-9UdniBc6T7nnGg  
-**文件名称：**A4--陈哲--从Ansible playbook到测试能力编排.pdf 分享链接:https://pan.baidu.com/s/1rbtmRDZmsmR5mT0R9WUfQQ  
-**文件名称：**A4--胡涛--从压测到剖析 - 中国银联性能测试实践(2).pdf 分享链接:https://pan.baidu.com/s/1Jd4y8QhEI4T7XqE5SnpmVQ  
-**文件名称：**A3--丁海苗--基于录制回放的智能化测试在金融场景的落地及实践.pdf 分享链接:https://pan.baidu.com/s/1YxU9PpudAj41C9KjX-A-mA  
-**文件名称：**A2--曾睦宁--广发银行信创项目技术测试实践分享(2).pdf 分享链接:https://pan.baidu.com/s/1ng_Ff68adsJEkhrj9mUu7g  
-**文件名称：**A4--陈哲--从Ansible playbook到测试能力编排(1).pdf 分享链接:https://pan.baidu.com/s/1Xpcy0qOZBt43m9wkNPqKXg  
-**文件名称：**A1--刘晓娟--百度单元测试智能生成实践.pdf 分享链接:https://pan.baidu.com/s/1tz7BqOQJ3oUgucs1bR7f8A  
-**文件名称：**A1--王海兵--基于流量回放的自动化回归测试平台AREX探索实践.pdf 分享链接:https://pan.baidu.com/s/1Gt29pbmgkprjY3hc0M4Bog  
-**文件名称：**A2-穆红伟-基础设施-企业级全栈测试平台.pdf 分享链接:https://pan.baidu.com/s/1caN92mb_a6xep0XvoZ39BA  
-**文件名称：**A3-余超-多云环境中部署及管理Kubernetes 集群的测试自动化实现(1).pdf 分享链接:https://pan.baidu.com/s/1djGRmVPcnIjgL65cYke7rA  
-**文件名称：**A4--胡涛--从压测到剖析 - 中国银联性能测试实践(1).pdf 分享链接:https://pan.baidu.com/s/11o5kcPY2CoPQYJfZRmwsXA  
-**文件名称：**A2--曾睦宁--广发银行信创项目技术测试实践分享(1).pdf 分享链接:https://pan.baidu.com/s/1mJ9rs8LbpajNDP09mg9WbA  
-**文件名称：**A2-穆红伟-基础设施-企业级全栈测试平台(1).pdf 分享链接:https://pan.baidu.com/s/1ept7Pbo8yXSWEyZ4tu-wUg  
-**文件名称：**A4--胡涛--从压测到剖析 - 中国银联性能测试实践.pdf 分享链接:https://pan.baidu.com/s/1t7SGlpyKC3-i4Tvr4TQDLA  
-**文件名称：**A1--王璐璐--坚持追求卓越 - 软件测试的自我革新与实践(1).pdf 分享链接:https://pan.baidu.com/s/1iVc4JTt5PlhxLvRxmX67bw  
-**文件名称：**A1--林冰玉--延伸测试边界，构建银行新型质量体系(2).pdf 分享链接:https://pan.baidu.com/s/1vw7KM8O7pDFMkE805JlkKQ  
-**文件名称：**A2--周海莲--基于AIGC的蚂蚁新一代测试用例自动生成技术(1).pdf 分享链接:https://pan.baidu.com/s/1tyMj945twYX3U-wQsmp9aw  
-**文件名称：**A3--丁海苗--基于录制回放的智能化测试在金融场景的落地及实践(2).pdf 分享链接:https://pan.baidu.com/s/1TrsK5c7TiA3U3wXuxtBQMQ  
-**文件名称：**A1--林冰玉--延伸测试边界，构建银行新型质量体系.pdf 分享链接:https://pan.baidu.com/s/199heu4MxHd9ca-tpmM4cLA  
-**文件名称：**A2--曾睦宁--广发银行信创项目技术测试实践分享.pdf 分享链接:https://pan.baidu.com/s/1G1XH2bTImRbzuomVklTAxg  
-**文件名称：**A1--王海兵--基于流量回放的自动化回归测试平台AREX探索实践(1).pdf 分享链接:https://pan.baidu.com/s/1afAJjxaHCQFo34bkm57ACA  
+- **文件名称:** 测试专场 分享链接:https://pan.baidu.com/s/1D-Q0Bkhd7wTk7q5j3xC6OA  
+- **文件名称:** A2--魏同权--基于大模型的软件测试能力提升研究.pdf 分享链接:https://pan.baidu.com/s/1otDDroWUbvueFf_6J3v0Pg  
+- **文件名称:** A2--魏同权--基于大模型的软件测试能力提升研究(1).pdf 分享链接:https://pan.baidu.com/s/1epUtJHnUZ0_g6jHd1vOAUw  
+- **文件名称:** A1--王璐璐--坚持追求卓越 - 软件测试的自我革新与实践.pdf 分享链接:https://pan.baidu.com/s/1nKwvN5oB1PSLWMS99gRhtQ  
+- **文件名称:** A3--丁海苗--基于录制回放的智能化测试在金融场景的落地及实践(1).pdf 分享链接:https://pan.baidu.com/s/12sLcEpnjgJ7HqyjxcBZpww  
+- **文件名称:** A1--林冰玉--延伸测试边界，构建银行新型质量体系(1).pdf 分享链接:https://pan.baidu.com/s/1-8jxnRpF-yc0EDL-knDgFg  
+- **文件名称:** A1--王璐璐--坚持追求卓越 - 软件测试的自我革新与实践(2).pdf 分享链接:https://pan.baidu.com/s/1DL5nhPKFRFrBtPTFQzxoBQ  
+- **文件名称:** A3-余超-多云环境中部署及管理Kubernetes 集群的测试自动化实现.pdf 分享链接:https://pan.baidu.com/s/1qUHSbmUlVRX4iB81m1u23A  
+- **文件名称:** A4--吕飞飞--基于LLM提升测试效率的应用实践(1).pdf 分享链接:https://pan.baidu.com/s/1qD7-avi7aSOpysSzFAerFA  
+- **文件名称:** 自动化测试专场--余超--多云环境中部署及管理K8S集群的测试自动化实现-脱敏(1).pdf 分享链接:https://pan.baidu.com/s/1jNASLJ5fvqiTQ8edxEIu7g  
+- **文件名称:** 自动化测试专场--余超--多云环境中部署及管理K8S集群的测试自动化实现-脱敏.pdf 分享链接:https://pan.baidu.com/s/1sSMhP3yNYKRYuhhtB-ydWw  
+- **文件名称:** A1-李柯-汽车数字转型下的智能精准测试.pdf 分享链接:https://pan.baidu.com/s/1wXjSP3Brx3YO3vTrD9UdBw  
+- **文件名称:** A2--周海莲--基于AIGC的蚂蚁新一代测试用例自动生成技术.pdf 分享链接:https://pan.baidu.com/s/1L7Tz80m2U3pM59xAR-q7jg  
+- **文件名称:** A4--吕飞飞--基于LLM提升测试效率的应用实践.pdf 分享链接:https://pan.baidu.com/s/15eAdNeH-9UdniBc6T7nnGg  
+- **文件名称:** A4--陈哲--从Ansible playbook到测试能力编排.pdf 分享链接:https://pan.baidu.com/s/1rbtmRDZmsmR5mT0R9WUfQQ  
+- **文件名称:** A4--胡涛--从压测到剖析 - 中国银联性能测试实践(2).pdf 分享链接:https://pan.baidu.com/s/1Jd4y8QhEI4T7XqE5SnpmVQ  
+- **文件名称:** A3--丁海苗--基于录制回放的智能化测试在金融场景的落地及实践.pdf 分享链接:https://pan.baidu.com/s/1YxU9PpudAj41C9KjX-A-mA  
+- **文件名称:** A2--曾睦宁--广发银行信创项目技术测试实践分享(2).pdf 分享链接:https://pan.baidu.com/s/1ng_Ff68adsJEkhrj9mUu7g  
+- **文件名称:** A4--陈哲--从Ansible playbook到测试能力编排(1).pdf 分享链接:https://pan.baidu.com/s/1Xpcy0qOZBt43m9wkNPqKXg  
+- **文件名称:** A1--刘晓娟--百度单元测试智能生成实践.pdf 分享链接:https://pan.baidu.com/s/1tz7BqOQJ3oUgucs1bR7f8A  
+- **文件名称:** A1--王海兵--基于流量回放的自动化回归测试平台AREX探索实践.pdf 分享链接:https://pan.baidu.com/s/1Gt29pbmgkprjY3hc0M4Bog  
+- **文件名称:** A2-穆红伟-基础设施-企业级全栈测试平台.pdf 分享链接:https://pan.baidu.com/s/1caN92mb_a6xep0XvoZ39BA  
+- **文件名称:** A3-余超-多云环境中部署及管理Kubernetes 集群的测试自动化实现(1).pdf 分享链接:https://pan.baidu.com/s/1djGRmVPcnIjgL65cYke7rA  
+- **文件名称:** A4--胡涛--从压测到剖析 - 中国银联性能测试实践(1).pdf 分享链接:https://pan.baidu.com/s/11o5kcPY2CoPQYJfZRmwsXA  
+- **文件名称:** A2--曾睦宁--广发银行信创项目技术测试实践分享(1).pdf 分享链接:https://pan.baidu.com/s/1mJ9rs8LbpajNDP09mg9WbA  
+- **文件名称:** A2-穆红伟-基础设施-企业级全栈测试平台(1).pdf 分享链接:https://pan.baidu.com/s/1ept7Pbo8yXSWEyZ4tu-wUg  
+- **文件名称:** A4--胡涛--从压测到剖析 - 中国银联性能测试实践.pdf 分享链接:https://pan.baidu.com/s/1t7SGlpyKC3-i4Tvr4TQDLA  
+- **文件名称:** A1--王璐璐--坚持追求卓越 - 软件测试的自我革新与实践(1).pdf 分享链接:https://pan.baidu.com/s/1iVc4JTt5PlhxLvRxmX67bw  
+- **文件名称:** A1--林冰玉--延伸测试边界，构建银行新型质量体系(2).pdf 分享链接:https://pan.baidu.com/s/1vw7KM8O7pDFMkE805JlkKQ  
+- **文件名称:** A2--周海莲--基于AIGC的蚂蚁新一代测试用例自动生成技术(1).pdf 分享链接:https://pan.baidu.com/s/1tyMj945twYX3U-wQsmp9aw  
+- **文件名称:** A3--丁海苗--基于录制回放的智能化测试在金融场景的落地及实践(2).pdf 分享链接:https://pan.baidu.com/s/1TrsK5c7TiA3U3wXuxtBQMQ  
+- **文件名称:** A1--林冰玉--延伸测试边界，构建银行新型质量体系.pdf 分享链接:https://pan.baidu.com/s/199heu4MxHd9ca-tpmM4cLA  
+- **文件名称:** A2--曾睦宁--广发银行信创项目技术测试实践分享.pdf 分享链接:https://pan.baidu.com/s/1G1XH2bTImRbzuomVklTAxg  
+- **文件名称:** A1--王海兵--基于流量回放的自动化回归测试平台AREX探索实践(1).pdf 分享链接:https://pan.baidu.com/s/1afAJjxaHCQFo34bkm57ACA  
 """, unsafe_allow_html=True)
 
 
